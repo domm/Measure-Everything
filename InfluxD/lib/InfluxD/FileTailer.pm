b614aadccc89e780caea5fc0ccb3eb1bcc2f9e44
Thomas Klausner <domm@plix.at>
2015-07-12 22:46:30 +0200

    InfluxD flush

--- InfluxD/lib/InfluxD/FileTailer.pm
+++ InfluxD/lib/InfluxD/FileTailer.pm
@@ -9,14 +9,19 @@ use IO::Async::Loop;
 use Hijk ();
 use Carp qw(croak);
 
-has 'file' => (is=>'ro',isa=>'Str',required=>1);
-has 'influx_host' => (is=>'ro',isa=>'Str',required=>1);
-has 'influx_port' => (is=>'ro',isa=>'Str',required=>1);
-has 'influx_db' => (is=>'ro',,isa=>'Str',required=>1);
+has 'file'        => ( is => 'ro', isa => 'Str', required => 1 );
+has 'influx_host' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'influx_port' =>
+    ( is => 'ro', isa => 'Int', default => 8086, required => 1 );
+has 'influx_db' => ( is => 'ro', isa => 'Str', required => 1 );
+
+has 'flush_size' =>
+    ( is => 'ro', isa => 'Int', required => 1, default => 100 );
+has 'flush_interval' =>
+    ( is => 'ro', isa => 'Int', required => 1, default => 5 );
+
 #TODO:
 # has tags = list of default tags that have to be added
-# flush_size = send to influx if buffered stats are bigger than
-# flush_interval = send to influx after this amount of time
 
 my @buffer;
 
@@ -24,23 +29,25 @@ sub run {
     my $self = shift;
     my $loop = IO::Async::Loop->new;
 
-    open(my $fh, "<", $self->file) || croak "Cannot open file ".$self->file.": $!";
+    open( my $fh, "<", $self->file )
+        || croak "Cannot open file " . $self->file . ": $!";
 
     my $filestream = IO::Async::FileStream->new(
         read_handle => $fh,
-        on_initial => sub {
-            my ( $self ) = @_;
-            $self->seek_to_last( "\n" );
+        on_initial  => sub {
+            my ($self) = @_;
+            $self->seek_to_last("\n");
         },
 
         on_read => sub {
             my ( $event, $buffref ) = @_;
 
-            while( $$buffref =~ s/^(.*\n)// ) {
-                push(@buffer, $1);
+            while ( $$buffref =~ s/^(.*\n)// ) {
+                #$self->send_direct($1);
+                push( @buffer, $1 );
             }
 
-            if (@buffer > 100) {
+            if ( @buffer > $self->flush_size ) {
                 $self->send;
             }
 
@@ -48,7 +55,18 @@ sub run {
         },
     );
 
-    $loop->add( $filestream );
+    $loop->add($filestream);
+
+    my $timer = IO::Async::Timer::Periodic->new(    # could be Countdown
+        interval => $self->flush_interval,
+
+        on_tick => sub {
+            say "Send periodic";
+            $self->send;
+        },
+    );
+    $timer->start;
+    $loop->add($timer);
 
     $loop->run;
 
@@ -56,19 +74,37 @@ sub run {
 
 sub send {
     my ($self) = @_;
-
-    my $res = Hijk::request({
-        method       => "POST",
-        host         => $self->influx_host,
-        port         => $self->influx_port,
-        path         => "/write",
-        query_string => "db=".$self->influx_db,
-        body         => join("\n",@buffer),
-    });
-    say "Sent buffer to influx";
-    @buffer=();
+    return unless @buffer;
+    say "Send buffer to influx size = " . scalar @buffer;
+    my $res = Hijk::request(
+        {   method       => "POST",
+            host         => $self->influx_host,
+            port         => $self->influx_port,
+            path         => "/write",
+            query_string => "db=" . $self->influx_db,
+            body         => join( "\n", @buffer ),
+        }
+    );
+    say "Sent!";
+    say $res->{status};
+    @buffer = ();
 }
 
+sub send_direct {
+    my ( $self, $line ) = @_;
+    my $res = Hijk::request(
+        {   method       => "POST",
+            host         => $self->influx_host,
+            port         => $self->influx_port,
+            path         => "/write",
+            query_string => "db=" . $self->influx_db,
+            body         => $line,
+        }
+    );
+    say "Sent!";
+    say $res->{status};
+
+}
 
 __PACKAGE__->meta->make_immutable;
 1;
