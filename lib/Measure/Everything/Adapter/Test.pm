55a3c47b5e9cc1273ef07174f5718582b09de0ab
Thomas Klausner <domm@plix.at>
2018-02-27 15:42:35 +0100

    add Test Adapter

---
+++ lib/Measure/Everything/Adapter/Test.pm
@@ -0,0 +1,58 @@
+package Measure::Everything::Adapter::Test;
+use strict;
+use warnings;
+
+use base qw(Measure::Everything::Adapter::Base);
+
+# ABSTRACT: Test Adapter: for testing...
+
+sub init {
+    my $self = shift;
+    $self->{_stats} = [];
+}
+
+sub write {
+    my $self = shift;
+
+    push(@{$self->{_stats}}, [@_]);
+}
+
+=method get_stats
+
+  my $stats = $stats->get_stats;
+
+Returns all the stats collected in the raw format (i.e. as an array).
+
+=cut
+
+sub get_stats {
+    my $self = shift;
+
+    return $self->{_stats};
+}
+
+=method reset
+
+  $stats->reset;
+
+Flushes all stats collected so far, starts from a clean slate.
+
+=cut
+
+sub reset {
+    my $self = shift;
+
+    $self->{_stats} = [];
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    Measure::Everything::Adapter->set( 'Test' );
+
+=head1 DESCRIPTION
+
+Collect stats in an in-memory array. Useful when you want to test if things are measured.
+
